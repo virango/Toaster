a94b1bcd6a6b1c79ee0c3f8eaec59a455fc15b87
tholan <tholan@2847db2f-2d22-4844-81d1-e0b81e2c12ed>
2015-10-17 15:01:00 +0000

    clean up MainForm (next round)

--- trunk/Toaster/MainFrame.cpp
+++ trunk/Toaster/MainFrame.cpp
@@ -31,12 +31,12 @@ MainFrame::MainFrame(QWidget *parent)
   connect(&mStompD, SIGNAL(onOffReceived(bool)), this, SLOT(onStompDOnOff(bool)));
   connect(&mStompX, SIGNAL(onOffReceived(bool)), this, SLOT(onStompXOnOff(bool)));
   connect(&mStompMod, SIGNAL(onOffReceived(bool)), this, SLOT(onStompModOnOff(bool)));
-  connect(&mDelay, SIGNAL(onOffCutsTailReceived(bool)), this, SLOT(onStompDelayOnOff(bool)));
-  connect(&mDelay, SIGNAL(feedbackReceived(double)), this, SLOT(onStompDelayFeedback(double)));
-  connect(&mDelay, SIGNAL(mixReceived(double)), this, SLOT(onStompDelayMix(double)));
-  connect(&mReverb, SIGNAL(onOffCutsTailReceived(bool)), this, SLOT(onStompReverbOnOff(bool)));
-  connect(&mReverb, SIGNAL(timeReceived(double)), this, SLOT(onStompReverbTime(double)));
-  connect(&mReverb, SIGNAL(mixReceived(double)), this, SLOT(onStompReverbMix(double)));
+  connect(&mDelay, SIGNAL(onOffCutsTailReceived(bool)), this, SLOT(onDelayOnOff(bool)));
+  connect(&mDelay, SIGNAL(feedbackReceived(double)), this, SLOT(onDelayFeedback(double)));
+  connect(&mDelay, SIGNAL(mixReceived(double)), this, SLOT(onDelayMix(double)));
+  connect(&mReverb, SIGNAL(onOffCutsTailReceived(bool)), this, SLOT(onReverbOnOff(bool)));
+  connect(&mReverb, SIGNAL(timeReceived(double)), this, SLOT(onReverbTime(double)));
+  connect(&mReverb, SIGNAL(mixReceived(double)), this, SLOT(onReverbMix(double)));
   connect(&mStompA, SIGNAL(typeReceived(::FXType)), this, SLOT(onStompAType(::FXType)));
   connect(&mStompB, SIGNAL(typeReceived(::FXType)), this, SLOT(onStompBType(::FXType)));
   connect(&mStompC, SIGNAL(typeReceived(::FXType)), this, SLOT(onStompCType(::FXType)));
@@ -68,19 +68,21 @@ MainFrame::MainFrame(QWidget *parent)
   connect(&mInput, SIGNAL(distortionSenseReceived(double)), SLOT(onInputDistortionSense(double)));
   connect(&mInput, SIGNAL(cleanSenseReceived(double)), SLOT(onInputCleanSense(double)));
 
-  ui->qStompAButton->setCtxMenuProvider(&mStompACtxMenu);
-  ui->qStompBButton->setCtxMenuProvider(&mStompBCtxMenu);
-  ui->qStompCButton->setCtxMenuProvider(&mStompCCtxMenu);
-  ui->qStompDButton->setCtxMenuProvider(&mStompDCtxMenu);
-  ui->qStompXButton->setCtxMenuProvider(&mStompXCtxMenu);
-  ui->qStompModButton->setCtxMenuProvider(&mStompModCtxMenu);
+  ui->stompAButton->setCtxMenuProvider(&mStompACtxMenu);
+  ui->stompBButton->setCtxMenuProvider(&mStompBCtxMenu);
+  ui->stompCButton->setCtxMenuProvider(&mStompCCtxMenu);
+  ui->stompDButton->setCtxMenuProvider(&mStompDCtxMenu);
+  ui->stompXButton->setCtxMenuProvider(&mStompXCtxMenu);
+  ui->stompModButton->setCtxMenuProvider(&mStompModCtxMenu);
 }
 
 MainFrame::~MainFrame()
 {
     delete ui;
 }
+//------------------------------------------------------------------------------------------
 
+// global communication
 void MainFrame::connect2KPA(const QString& connectName)
 {
   mGlobal.connect2KPA(connectName);
@@ -114,6 +116,7 @@ void MainFrame::requestValues()
   //DebugMidi::get().debugScanRequest(0x01, 0x00, 0x7F);
 }
 
+// utilities
 void MainFrame::handleStompButtonClick(Stomp& stomp, QToasterButton& stompBt, bool longClick)
 {
   if(longClick)
@@ -152,105 +155,80 @@ void MainFrame::toggleOperationMode(OperationMode opMode, QToasterButton* bt)
     mEditModeButton = NULL;
   }
 }
+//------------------------------------------------------------------------------------------
 
-/*
- * stomps slots
- */
-void MainFrame::on_qStompAButton_clicked(QToasterButton& bt, bool longClick)
+// stomps
+// ui => kpa
+void MainFrame::on_stompAButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompA, bt, longClick);
 }
 
-void MainFrame::on_qStompBButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompBButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompB, bt, longClick);
 }
 
-void MainFrame::on_qStompCButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompCButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompC, bt, longClick);
 }
 
-void MainFrame::on_qStompDButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompDButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompD, bt, longClick);
 }
 
-void MainFrame::on_qStompXButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompXButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompX, bt, longClick);
 }
 
-void MainFrame::on_qStompModButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompModButton_clicked(QToasterButton& bt, bool longClick)
 {
   handleStompButtonClick(mStompMod, bt, longClick);
 }
 
+// kpa => ui
 void MainFrame::onStompAOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompAButton->setState(state);
+  ui->stompAButton->setState(state);
   update();
 }
 
 void MainFrame::onStompBOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompBButton->setState(state);
+  ui->stompBButton->setState(state);
   update();
 }
 
 void MainFrame::onStompCOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompCButton->setState(state);
+  ui->stompCButton->setState(state);
   update();
 }
 
 void MainFrame::onStompDOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompDButton->setState(state);
+  ui->stompDButton->setState(state);
   update();
 }
 
 void MainFrame::onStompXOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompXButton->setState(state);
+  ui->stompXButton->setState(state);
   update();
 }
 
 void MainFrame::onStompModOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompModButton->setState(state);
-  update();
-}
-
-void MainFrame::onStompDelayOnOff(bool onOff)
-{
-  QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompDelayButton->setState(state);
-  update();
-}
-
-void MainFrame::onStompDelayFeedback(double feedback)
-{
-  ui->qDelayFeedbackDial->setValue(feedback);
-  update();
-}
-
-void MainFrame::onStompDelayMix(double mix)
-{
-  ui->qDelayMixDial->setValue(mix);
-  update();
-}
-
-void MainFrame::onStompReverbOnOff(bool onOff)
-{
-  QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompReverbButton->setState(state);
+  ui->stompModButton->setState(state);
   update();
 }
 
@@ -283,17 +261,53 @@ void MainFrame::onStompModType(::FXType type)
 {
   // TODO: set the LED color
 }
+//------------------------------------------------------------------------------------------
+
+// reverb
+// ui => kpa
+
+// kpa => ui
+void MainFrame::onReverbOnOff(bool onOff)
+{
+  QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
+  ui->reverbButton->setState(state);
+  update();
+}
+
+// delay
+// ui => kpa
+
+// kpa => ui
+void MainFrame::onDelayOnOff(bool onOff)
+{
+  QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
+  ui->delayButton->setState(state);
+  update();
+}
+
+void MainFrame::onDelayFeedback(double feedback)
+{
+  ui->delayFeedbackDial->setValue(feedback);
+  update();
+}
+
+void MainFrame::onDelayMix(double mix)
+{
+  ui->delayMixDial->setValue(mix);
+  update();
+}
 
 
-/*
- * amp slots
- */
-void MainFrame::on_qGainDial_valueChanged(double gain)
+//------------------------------------------------------------------------------------------
+
+
+// amp
+void MainFrame::on_gainDial_valueChanged(double gain)
 {
   mAmp.applyGain(gain);
 }
 
-void MainFrame::on_qAmplifierButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_amplifierButton_clicked(QToasterButton& bt, bool longClick)
 {
   if(longClick)
     toggleOperationMode(AmpEdit, &bt);
@@ -305,85 +319,89 @@ void MainFrame::on_qAmplifierButton_clicked(QToasterButton& bt, bool longClick)
 void MainFrame::onAmpOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qAmplifierButton->setState(state);
+  ui->amplifierButton->setState(state);
   update();
 }
 
 void MainFrame::onAmpGain(double val)
 {
-  ui->qGainDial->setValue(val);
+  ui->gainDial->setValue(val);
   update();
 }
+//------------------------------------------------------------------------------------------
 
+// eq
 void MainFrame::onEqOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qEQButton->setState(state);
+  ui->eqButton->setState(state);
   update();
 }
 
-void MainFrame::on_qVolumeDial_valueChanged(double physVal)
+
+// rig
+void MainFrame::on_volumeDial_valueChanged(double physVal)
 {
   mRig.applyVolume(physVal);
 }
 
 void MainFrame::onRigVolume(double volume)
 {
-  ui->qVolumeDial->setValue(volume);
+  ui->volumeDial->setValue(volume);
   update();
 }
 
 void MainFrame::onRigStompsEnable(bool stompsEnable)
 {
   QToasterButton::State state = stompsEnable ? QToasterButton::On : QToasterButton::Off;
-  ui->qStompsButton->setState(state);
+  ui->stompsButton->setState(state);
   update();
 }
 
 void MainFrame::onRigStackEnable(bool stackEnable)
 {
   QToasterButton::State state = stackEnable ? QToasterButton::On : QToasterButton::Off;
-  ui->qStackButton->setState(state);
+  ui->stackButton->setState(state);
   update();
 }
 
 void MainFrame::onRigEffectsEnable(bool effectsEnable)
 {
   QToasterButton::State state = effectsEnable ? QToasterButton::On : QToasterButton::Off;
-  ui->qEffectsButton->setState(state);
+  ui->effectsButton->setState(state);
   update();
 }
 
-void MainFrame::on_qChickenHeadDial_valueChanged(const QChickenHeadDial::State& state)
+void MainFrame::on_chickenHeadDial_valueChanged(const QChickenHeadDial::State& state)
 {
   mGlobal.applyOperationMode((Global::OperationMode) state);
 }
 
 void MainFrame::onGlobalOperationMode(unsigned short opMode)
 {
-  ui->qChickenHeadDial->setState((QChickenHeadDial::State)opMode);
+  ui->chickenHeadDial->setState((QChickenHeadDial::State)opMode);
   update();
 }
 
-void MainFrame::on_qStompsButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stompsButton_clicked(QToasterButton& bt, bool longClick)
 {
   mRig.applyStompsEnable(bt.toggleOnOff());
   update();
 }
 
-void MainFrame::on_qStackButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_stackButton_clicked(QToasterButton& bt, bool longClick)
 {
   mRig.applyStackEnable(bt.toggleOnOff());
   update();
 }
 
-void MainFrame::on_qEffectsButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_effectsButton_clicked(QToasterButton& bt, bool longClick)
 {
   mRig.applyEffectsEnable(bt.toggleOnOff());
   update();
 }
 
-void MainFrame::on_qEQButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_eqButton_clicked(QToasterButton& bt, bool longClick)
 {
   if(longClick)
     toggleOperationMode(EQEdit, &bt);
@@ -395,11 +413,11 @@ void MainFrame::on_qEQButton_clicked(QToasterButton& bt, bool longClick)
 void MainFrame::onCabOnOff(bool onOff)
 {
   QToasterButton::State state = onOff ? QToasterButton::On : QToasterButton::Off;
-  ui->qCabinetButton->setState(state);
+  ui->cabinetButton->setState(state);
   update();
 }
 
-void MainFrame::on_qCabinetButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_cabinetButton_clicked(QToasterButton& bt, bool longClick)
 {
   if(longClick)
     toggleOperationMode(CabEdit, &bt);
@@ -408,12 +426,12 @@ void MainFrame::on_qCabinetButton_clicked(QToasterButton& bt, bool longClick)
   update();
 }
 
-void MainFrame::on_qMonitorVolumeDial_valueChanged(double volume)
+void MainFrame::on_monitorVolumeDial_valueChanged(double volume)
 {
   mGlobal.applyMonitorOutputVolume(volume);
 }
 
-void MainFrame::on_qHeadphoneVolumeDial_valueChanged(double volume)
+void MainFrame::on_headphoneVolumeDial_valueChanged(double volume)
 {
   mGlobal.applyHeadphoneOutputVolume(volume);
 }
@@ -425,13 +443,13 @@ void MainFrame::onGlobalMainVolume(double volume)
 
 void MainFrame::onGlobalHeadphoneVolume(double volume)
 {
-  ui->qHeadphoneVolumeDial->setValue(volume);
+  ui->headphoneVolumeDial->setValue(volume);
   update();
 }
 
 void MainFrame::onGlobalMonitorVolume(double volume)
 {
-  ui->qMonitorVolumeDial->setValue(volume);
+  ui->monitorVolumeDial->setValue(volume);
   update();
 }
 
@@ -440,40 +458,40 @@ void MainFrame::onGlobalDirectVolume(double volume)
   update();
 }
 
-void MainFrame::on_qNoiseGateDial_valueChanged(double noiseGate)
+void MainFrame::on_noiseGateDial_valueChanged(double noiseGate)
 {
   mInput.applyNoiseGate(noiseGate);
 }
 
-void MainFrame::on_qDistortionSenseDial_valueChanged(double distortionSense)
+void MainFrame::on_distortionSenseDial_valueChanged(double distortionSense)
 {
   mInput.applyDistortionSense(distortionSense);
 }
 
-void MainFrame::on_qCleanSenseDial_valueChanged(double cleanSense)
+void MainFrame::on_cleanSenseDial_valueChanged(double cleanSense)
 {
   mInput.applyCleanSense(cleanSense);
 }
 
 void MainFrame::onInputNoiseGate(double noiseGate)
 {
-  ui->qNoiseGateDial->setValue(noiseGate);
+  ui->noiseGateDial->setValue(noiseGate);
   update();
 }
 
 void MainFrame::onInputDistortionSense(double distortionSense)
 {
-  ui->qDistortionSenseDial->setValue(distortionSense);
+  ui->distortionSenseDial->setValue(distortionSense);
   update();
 }
 
 void MainFrame::onInputCleanSense(double cleanSense)
 {
-  ui->qCleanSenseDial->setValue(cleanSense);
+  ui->cleanSenseDial->setValue(cleanSense);
   update();
 }
 
-void MainFrame::on_qRigPrevButton_clicked(QToasterButton &bt, bool longClick)
+void MainFrame::on_rigPrevButton_clicked(QToasterButton &bt, bool longClick)
 {
   if(bt.state() == QToasterButton::On)
   {
@@ -482,7 +500,7 @@ void MainFrame::on_qRigPrevButton_clicked(QToasterButton &bt, bool longClick)
   }
 }
 
-void MainFrame::on_qRigNextButton_clicked(QToasterButton &bt, bool longClick)
+void MainFrame::on_rigNextButton_clicked(QToasterButton &bt, bool longClick)
 {
   if(bt.state() == QToasterButton::On)
   {
@@ -491,7 +509,7 @@ void MainFrame::on_qRigNextButton_clicked(QToasterButton &bt, bool longClick)
   }
 }
 
-void MainFrame::on_qStompDelayButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_delayButton_clicked(QToasterButton& bt, bool longClick)
 {
   if(longClick)
   {
@@ -504,7 +522,7 @@ void MainFrame::on_qStompDelayButton_clicked(QToasterButton& bt, bool longClick)
   update();
 }
 
-void MainFrame::on_qStompReverbButton_clicked(QToasterButton& bt, bool longClick)
+void MainFrame::on_reverbButton_clicked(QToasterButton& bt, bool longClick)
 {
   if(longClick)
   {
@@ -517,32 +535,32 @@ void MainFrame::on_qStompReverbButton_clicked(QToasterButton& bt, bool longClick
   update();
 }
 
-void MainFrame::on_qDelayFeedbackDial_valueChanged(double arg1)
+void MainFrame::on_delayFeedbackDial_valueChanged(double arg1)
 {
   mDelay.applyFeedback(arg1);
 }
 
-void MainFrame::on_qDelayMixDial_valueChanged(double arg1)
+void MainFrame::on_delayMixDial_valueChanged(double arg1)
 {
     mDelay.applyMix(arg1);
 }
 
-void MainFrame::on_qReverbTimeDial_valueChanged(double arg1)
+void MainFrame::on_reverbTimeDial_valueChanged(double arg1)
 {
   mReverb.applyTime(arg1);
 }
 
-void MainFrame::on_qReverbMixDial_valueChanged(double arg1)
+void MainFrame::on_reverbMixDial_valueChanged(double arg1)
 {
   mReverb.applyMix(arg1);
 }
 
-void MainFrame::onStompReverbTime(double time)
+void MainFrame::onReverbTime(double time)
 {
-  ui->qReverbTimeDial->setValue(time);
+  ui->reverbTimeDial->setValue(time);
 }
 
-void MainFrame::onStompReverbMix(double mix)
+void MainFrame::onReverbMix(double mix)
 {
-  ui->qReverbMixDial->setValue(mix);
+  ui->reverbMixDial->setValue(mix);
 }
